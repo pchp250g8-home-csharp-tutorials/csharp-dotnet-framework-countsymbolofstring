a63a6e9d1c28523339fe6154972df16bf317a400
Alexander <pchp250g8@tutamail.com>
2026-04-22 12:06:50 +0300

    Add project files.

---
+++ CountSymbolOfString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountSymbolOfString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input a string");
+            var strLine = Console.ReadLine();
+            Console.WriteLine("Input a symbol");
+            var c = 0;
+            var chSym = Console.Read();
+            var nLen = strLine.Length;
+            for (int i = 0; i < nLen; i++)
+            {
+                if (strLine[i] == chSym)
+                {
+                    c++;
+                }
+            }
+            var fltPart = ((float)c / nLen) * 100f;
+            Console.WriteLine("The symbol {0} occurs {1} times", (char)chSym, c);
+            Console.WriteLine
+            (
+                "The symbol \"{0}\" occupies {1}% of the string \"{2}\"",
+                (char)chSym, fltPart, strLine
+            );
+        }
+    }
+}
